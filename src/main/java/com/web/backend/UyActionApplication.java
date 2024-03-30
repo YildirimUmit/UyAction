8ed6b5c627debf7332d77f1df7139535d4a6b59b
Ümit Yıldırım <umityild@gmail.com>
2024-03-30 22:00:11 +0300

    Initial commit Hello Word :)

---
+++ src/main/java/com/web/backend/UyActionApplication.java
@@ -0,0 +1,13 @@
+package com.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UyActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UyActionApplication.class, args);
+    }
+
+}
