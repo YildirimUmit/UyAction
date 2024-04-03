2e68bc97707e6f2895386c15786100ffcf0ff7d3
Ümit Yıldırım <umityild@gmail.com>
2024-04-03 21:52:50 +0300

    fix

--- src/main/java/com/web/backend/UyActionApplication.java
+++ src/main/java/com/web/backend/UyActionApplication.java
@@ -2,9 +2,10 @@ package com.web.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class UyActionApplication {
 
     public static void main(String[] args) {
